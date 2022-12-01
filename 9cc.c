4da4cfe53eb96e306f89c1ee0a78eeb8bd4332e2
rg_thikpad <ryotagoto8@gmail.com>
2022-12-01 15:19:56 +0900

    単項マイナス

--- 9cc.c
+++ 9cc.c
@@ -160,6 +160,7 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr() {
@@ -176,13 +177,13 @@ Node *expr() {
 }
 
 Node *mul() {
-	Node *node = primary();
+	Node *node = unary();
 
 	for (;;) {
 		if (consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if (consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return node;
 	}
@@ -198,6 +199,14 @@ Node *primary() {
 
 	// そうでなければ数値のはず
 	return new_node_num(expect_number());
+}
+
+Node *unary() {
+	if (consume('+'))
+		return primary();
+	if (consume('-'))
+		return new_node(ND_SUB, new_node_num(0), primary());
+	return primary();
 }
 
 void gen(Node *node) {
